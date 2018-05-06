7792e63eab864e2b9b648ff2cdc6c2c3b509c486
daniel reyes <danielreyes@MacBook-Pro-de-daniel.local>
2018-05-06 11:09:34 -0300

    Adição de normalização de dados, gráfico de barras para predição de apenas um passo

--- Regressão_Feature_Engineering.py
+++ Regressão_Feature_Engineering.py
@@ -21,20 +21,19 @@ from pandas import DataFrame
 RANGE_N = 11.0
 TAM_PASSO = 0.01
 DIVISOR = 100
-QT_PASSO_PRED = 10
+QT_PASSO_PRED = 1
 TAM_JANELA = 15
 
 #Função para criar um data set e posteriormente dividi-lo em treino e teste
 #Dentro da função há a equação que produzirá os valores
-def monta_dataset(RANGE_N, TAM_PASSO):
+def monta_dataset(RANGE_N):
     pontos = []
-    valores_n = []
-    n_passos = int(RANGE_N / TAM_PASSO)
-    for n in range(n_passos):
-        result = math.sqrt(1 + (math.sin(n + (math.sin(n) ** 2))))
-        pontos.append(result)
-        value_n = n/DIVISOR
-        valores_n.append(value_n)
+    valores_n = []    
+    n_passos = pd.Series(np.arange(0.0, RANGE_N, TAM_PASSO))
+    for n in n_passos:
+        result = math.sqrt(1.0 + (math.sin(n + (math.sin(n) ** 2.0))))
+        pontos.append(float(result))
+        valores_n.append(float(n))
     return valores_n, pontos
 
 def monta_cabecalho_janela():
@@ -73,7 +72,27 @@ def metricas(y_real, predicoes):
     #Calcula a métrica de variância de predições
     print('Variance score: %.2f' % r2_score(y_real, predicoes))
 
-X, y = monta_dataset(11, 0.01)
+def plota_grafico_1passo(y_real, y_predito, titulo):
+    y_real = np.array(y_real)    
+    y_predito = np.array(y_predito)
+    
+    objects = ('Real', 'Predito')
+    x_pos = np.arange(len(objects))
+    performance = [y_real,y_predito]
+    color = ['green', 'red']
+
+    plt.bar(x_pos, performance, align='center', alpha=0.5, color=color)
+    plt.xticks(x_pos, objects, fontsize=12, rotation=30)
+    plt.xlabel('Valor do próximo passo', fontsize=12)
+    plt.title(titulo)
+     
+    plt.show()
+
+X, y = monta_dataset(RANGE_N)
+#df_y = pd.Series(y)
+#df_x = pd.Series(X)
+#df_y.plot()
+
 
 df_pontos = DataFrame(pd.Series(y).values)
 
@@ -104,12 +123,17 @@ rna = MLPRegressor(hidden_layer_sizes=(100, 100),
                    activation='tanh', 
                    early_stopping=True)
 
-#print(df_pontos.head(2))
 #Cria um dataframe apenas com as variáveis, excluindo os rótulos do momento 'x' a ser predito posteriormente
 features = df_pontos.drop('x', axis=1)
-#print(df_pontos_x.head(2))
+
 #Cria um dataframe\lista com os rótulos reais
 labels = df_pontos['x']
+
+#Declara um scaler, para normalizar os dados entre -1 e 1
+max_abs_scaler = MaxAbsScaler()
+features = max_abs_scaler.fit_transform(features)
+features = DataFrame(features)
+#max_abs_scaler = max_abs_scaler.fit(features)
 
 #treina o modelo com o dataframe de variáveis e os rótulos
 rna.fit(features, labels)
@@ -118,11 +142,16 @@ rna.fit(features, labels)
 features_to_test_model = features.tail(QT_PASSO_PRED)
 predicoes = rna.predict(features_to_test_model)
 
-plota_grafico_2(labels.tail(QT_PASSO_PRED),
-              labels.tail(QT_PASSO_PRED), 
-              predicoes, 
-              "Predição de " + str(QT_PASSO_PRED) + " passo(s) \n com deslocamento temporal", 
-              QT_PASSO_PRED)
+if(QT_PASSO_PRED==1):
+    plota_grafico_1passo(labels.tail(QT_PASSO_PRED),
+                         predicoes,
+                         "Predição de " + str(QT_PASSO_PRED) + " passo(s) \n com deslocamento temporal")
+else:    
+    plota_grafico_2(labels.tail(QT_PASSO_PRED),
+                  labels.tail(QT_PASSO_PRED), 
+                  predicoes, 
+                  "Predição de " + str(QT_PASSO_PRED) + " passo(s) \n sem deslocamento temporal. Fase: Treino", 
+                  QT_PASSO_PRED)
 
 #Agora mensura o quanto o modelo acertou com os dados de treino
 metricas(labels.tail(QT_PASSO_PRED), predicoes)
@@ -132,6 +161,8 @@ predicoes_passos = []
 #Criando um dataset para guardar os valores que foram utilizados para predizer os próximos N passos
 df_teste = DataFrame()
 
+#Monta o dataset de treino para os testes
+#Pega o dataset original, menos a quantidade de dados pra predição, menos a quantidade de dados da janela
 df_treino = features.head(len(X) - QT_PASSO_PRED-TAM_JANELA)
 y_treino = labels.head(len(X) - QT_PASSO_PRED-TAM_JANELA)
 
@@ -139,49 +170,69 @@ for i in range(QT_PASSO_PRED):
     #Monta a primeira linha de dados de entrada para serem preditos.
     #Pega os dados da ultima linha de treino e junta com o resultado da ultima linha de treino
     if(i==0):
-        
-#        ultimo_y_real = df_pontos_y.tail(1)
-        
-#        arr_tmp = df_pontos_x.tail(1).values
-        primeiralinha = df_treino.tail(1)
 
-        ultimo_y_real = y_treino.tail(1)
-        
+#        Vamos guardar os dados que foram usados como dataset de teste
+#        Como ele está vazio, caso seja a primeira linha, irá pegar a ultima linha de dados
+#        do dataset de treino e o seu rótulo
+#        Transformo em array só para poder manuseá-lo
         arr_tmp = df_treino.tail(1)
-        
+        ultimo_y_real = y_treino.tail(1)        
+
+#       Valor a ser predito é o instante x
+#       Dessa forma, adiciono o ultimo valor a linha como se fosse o instante x-1
         print("Adiciona o ultimo valor (rotulo) dos dados de treino: (%f) " % ultimo_y_real)
         arr_tmp = np.append(arr_tmp, ultimo_y_real)
+
+#       Agora removo o valor do último instante que conhecemos
         print("Faz o shift deletando o primeiro valor da coluna valor: (%f)" % arr_tmp[0])
         arr_tmp = np.delete(arr_tmp, 0)
-        
+
+#       Transforma em uma linha de dataframe pra poder concatenar no dataframe de teste
         df_tmp = pd.Series(arr_tmp).to_frame().transpose()
         df_tmp.columns = features.columns
+        df_tmp = pd.DataFrame(max_abs_scaler.transform(df_tmp))
         df_teste = df_teste.append(df_tmp, ignore_index=True)
-        
+
+#   Até agora não houve nenhuma predição dos dados de teste
+#   Pegamos a última linha do dataframe de teste pois é o próximo passo que deve ser predito
     dado_to_predict = df_teste.tail(1)
-    
+
+#   Prediz o próximo passo da série temporal    
     valor_predito = rna.predict(dado_to_predict)
 #    print("Valor (%d) Predito: " % i)
 #    print(valor_predito)
-    
+
+#   Pegamos a última linha de teste (a mesma usada para predição) para poder manuseá-la    
 #    print("Pega a última linha dos dados de treino e transforma em um array: ")
     arr_tmp = df_teste.tail(1).values
-    
+
+#   Adiciona o valor predito à linha    
 #    print("Adiciona o ultimo valor (rotulo) predito: (%f) " % valor_predito)
     arr_tmp = np.append(arr_tmp, valor_predito)
     
+#   Faz o shift, deletando o valor do instante mais antigo que se conhece da linha
 #    print("Faz o shift deletando o primeiro valor da linha de teste: (%f)" % arr_tmp[0])
     arr_tmp = np.delete(arr_tmp, 0)
+
+#   Transforma o array em uma linha de dataframe para poder concatenar ao dataframe de teste
     df_tmp = pd.Series(arr_tmp).to_frame().transpose()
-    df_tmp.columns = colunas
+    df_tmp.columns = features.columns
+    df_tmp = pd.DataFrame(max_abs_scaler.transform(df_tmp))
     df_teste = df_teste.append(df_tmp, ignore_index=True)
     
+#   Guarda em uma lista para poder mensurar o modelo:
 #    print("Guarda em uma lista para poder mensurar o modelo:")
     predicoes_passos.append(valor_predito)
 
 #df_teste = df_teste.drop(df_teste.index[0])
     
-plota_grafico_2(labels.tail(QT_PASSO_PRED),
+#plota_grafico_2(labels.tail(QT_PASSO_PRED),
+if(QT_PASSO_PRED == 1):
+    plota_grafico_1passo(labels.tail(QT_PASSO_PRED), 
+                         predicoes_passos, 
+                         "Predição de " + str(QT_PASSO_PRED) + " passo(s) \n com deslocamento temporal")
+else:
+    plota_grafico_2(y,
           labels.tail(QT_PASSO_PRED), 
           predicoes_passos, 
           "Predição de " + str(QT_PASSO_PRED) + " passo(s) \n com deslocamento temporal", 
